ac8bc07e9c6ce399c5664be6827092449b6cfee1
Randy <chiurandyboy27@gmail.com>
2020-10-16 19:11:36 +0800

    jave and kolint test 2

--- src/main/java/com/zero/Hellword.java
+++ src/main/java/com/zero/Hellword.java
@@ -4,5 +4,9 @@ public class Hellword {
     public static void main(String[] args) {
         Sayhello p = new Sayhello();
         p.hello();
+        int a = 9;
+        int b = 12;
+        int c= a+b;
+        System.out.println(c);
     }
 }
--- src/main/kotlin/com/zero/Hello.kt
+++ src/main/kotlin/com/zero/Hello.kt
@@ -12,12 +12,16 @@ fun main(args: Array<String>) {
     val b = Sayhello()
     a.hello()
     b.hello()
+    val c = 9;
+    val d = 10;
+    val e = c*d;
+    println(e)
 }
 
 // 類別 類別名稱{} kolint跟java是可以合併再一起的，所以也可以使用java的東西
 class Ksayhellow{
 //  方法 方法名稱
     fun hello(){
-        println("Starbrust Stream kolint")
+        println("Starbrust Stream kolint yayaya")
     }
 }
